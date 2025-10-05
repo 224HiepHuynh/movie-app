1e5f3e2a8937f688a1ad1b098eb567565cc31575
224hiephuynh <224hiephuynh@gmail.com>
2025-10-04 23:30:42 -0400

    added some configurations

--- movie/src/main/java/com/job/movie/MovieApplication.java
+++ movie/src/main/java/com/job/movie/MovieApplication.java
@@ -1,13 +1,21 @@
 package com.job.movie;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.job.movie.config.TmdbConfig;
 
 @SpringBootApplication
 public class MovieApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
+		
+			
 	}
+
 
 }
